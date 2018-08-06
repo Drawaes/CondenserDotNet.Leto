bea52a21ad3951bc0ea5606c63f018d0d985de4b
Tim Seaward <seawardtim@gmail.com>
2018-08-06 02:10:31 +0100

    Added interop file that was missing

---
+++ src/CondenserDotNet.Leto.EphemeralBuffers/Interop/Unix/MAdvise.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CondenserDotNet.Leto.EphemeralBuffers.Interop.Unix
+{
+    internal static partial class Interop
+    {
+        [DllImport(Libraries.LibC, EntryPoint = "madvise")]
+        internal static extern int MAdvise(IntPtr ptr, long length, Advice advice);
+
+        public enum Advice : int
+        {
+            MADV_DONTDUMP = 16,
+            MADV_DODUMP = 17,
+        }
+    }
+}
